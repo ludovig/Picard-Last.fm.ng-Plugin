60568874367ec5c5232b35791ef8fecdcc528057
Ludovic Vigouroux <ludo@mundoludo.fr>
2018-03-04 14:16:03 +0100

    Adapt lastfmng to PyQt5 + remove « unknown » information

--- lastfmng/logging.py
+++ lastfmng/logging.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals, absolute_import
-
 import logging
 
 from picard import log as picard_log
@@ -9,15 +7,14 @@ from picard import log as picard_log
 class PicardHandler(logging.Handler):
     def emit(self, record):
         levels = {
-            10: picard_log.LOG_DEBUG,
-            20: picard_log.LOG_INFO,
-            30: picard_log.LOG_WARNING,
-            40: picard_log.LOG_ERROR,
-            50: picard_log.LOG_ERROR,
+            10: logging.DEBUG,
+            20: logging.INFO,
+            30: logging.WARNING,
+            40: logging.ERROR,
+            50: logging.ERROR,
         }
-        level = levels.get(record.levelno, picard_log.LOG_DEBUG)
+        level = levels.get(record.levelno, logging.DEBUG)
         message = '{} - {}'.format('Last.fm.ng', record.msg)
-        picard_log.main_logger.message(level, message, *record.args)
 
 
 def setup_logging():
